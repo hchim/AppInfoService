0e463d8244e9517a2e48311cca21ef8c3737c9cb
hchen <hchen229@gmail.com>
2017-02-27 21:50:36 -0800

    Fix the read error of nested parameters.

--- routes/suggestions.js
+++ routes/suggestions.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var Suggestion = require("../models/Suggestion");
+var commonUtils = require('servicecommonutils')
 
 /*
 * Add suggestion
 */
 router.post("/", function(req, res, next) {
-  var model = req.body['device[model]'];
-  var brand = req.body['device[brand]'];
-  var serial = req.body['device[serial]'];
-  var os_name = req.body['os[os_name]'];
-  var sdk_int = req.body['os[sdk_int]'];
+  var model = commonUtils.nestedReqField(req.body, 'device', 'model')
+  var brand = commonUtils.nestedReqField(req.body, 'device', 'brand')
+  var serial = commonUtils.nestedReqField(req.body, 'device', 'serial')
+  var os_name = commonUtils.nestedReqField(req.body, 'os', 'os_name')
+  var sdk_int = commonUtils.nestedReqField(req.body, 'os', 'sdk_int')
   var sdk_int_val = parseInt(sdk_int);
-  var os_type = req.body['os[os_type]'];
-  var fingerprint = req.body['os[fingerprint]'];
+  var os_type = commonUtils.nestedReqField(req.body, 'os', 'os_type')
+  var fingerprint = commonUtils.nestedReqField(req.body, 'os', 'fingerprint')
 
   var suggestion = new Suggestion({
     userId: req.body.userId,
