448b34ad84d2b596c7ece05b62f654cda3c091ff
hchen <hchen229@gmail.com>
2017-08-03 21:33:56 -0700

    Remove log rotator

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
-var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var FileStreamRotator = require('file-stream-rotator');
 var fs = require('fs');
 var conf = require("./config");
 var metric = require('metricsclient')(conf)
@@ -18,21 +16,8 @@ var appconfigs = require('./routes/appconfigs')
 
 var app = express();
 
-if (conf.get("env") === "production") {
-    var logDirectory = __dirname + '/log';
-    // ensure log directory exists
-    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-    // create a rotating write stream
-    var accessLogStream = FileStreamRotator.getStream({
-        date_format: conf.get('log.dateformat'),
-        filename: logDirectory + '/access-%DATE%.log',
-        frequency: conf.get("log.frequency"),
-        verbose: false
-    });
-    app.use(morgan('combined', {stream: accessLogStream}));
-} else {
-    app.use(morgan('dev'));
-}
+var logDirectory = __dirname + '/log';
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
