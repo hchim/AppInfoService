caf1d7efa7ce075b60a4bdc4997575067cadca36
hchen <hchen229@gmail.com>
2017-08-13 02:48:38 -0700

    Update handlers

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var conf = require("./config");
-var metric = require('metricsclient')(conf)
 var middlewares = require('service-middlewares')(conf)
-var utils = require('servicecommonutils')
-
-var winston = utils.getWinston(conf.get('env'));
 
 //routes
 var index = require('./routes/index');
@@ -15,7 +11,6 @@ var suggestions = require('./routes/suggestions');
 var appconfigs = require('./routes/appconfigs')
 
 var app = express();
-
 var logDirectory = __dirname + '/log';
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
@@ -32,43 +27,11 @@ if (conf.get("env") !== 'test') {
 // setup routes
 app.use('/suggestions', suggestions);
 
-//catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('404 Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stack trace
-if (conf.get("env") === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.json(utils.encodeResponseBody(req, {
-            message: err.message,
-            error: err,
-            errorCode: 'INTERNAL_FAILURE'
-        }));
-    });
+app.use(middlewares.error_404_middleware);
+if (conf.get("env") !== 'production') {
+    app.use(middlewares.error_500_middleware_dev);
+} else {
+    app.use(middlewares.error_500_middleware_prod)
 }
-
-// production error handler
-// no stack traces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    metric.errorMetric('AppInfoService:Error:' + req.method + ':' + req.url, err, function (error, jsonObj) {
-        if (error != null)
-            return res.json(utils.encodeResponseBody(req, {
-                message: 'Failed to add metric. \n' + err.message,
-                errorCode: 'INTERNAL_FAILURE'
-            }));
-        res.json(utils.encodeResponseBody(req, {
-            message: err.message,
-            errorCode: 'INTERNAL_FAILURE'
-        }));
-    })
-});
 
 module.exports = app;
